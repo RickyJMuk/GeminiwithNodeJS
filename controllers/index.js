c1de4b987364846e9768c3dbfe57701d38ef0e9d
RickyJMuk <rickymuk254@gmail.com>
2024-05-09 18:54:42 +0300

    my initial commit

---
+++ controllers/index.js
@@ -0,0 +1,51 @@
+// Instead of export const history = [];
+const history = [];
+module.exports = { history };
+
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// GoogleGenerativeAI required config
+const configuration = new GoogleGenerativeAI(process.env.API_KEY);
+
+// Model initialization
+const modelId = "gemini-pro";
+const generationConfig = {
+  stopSequences: ["red"],
+  maxOutputTokens: 200,
+  temperature: 0.9,
+  topP: 0.1,
+  topK: 16,
+};
+
+const model = configuration.getGenerativeModel({ model: modelId, generationConfig });
+
+/**
+ * Generates a response based on the given prompt.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise} - A promise that resolves when the response is sent.
+ */
+
+const generateResponse = async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+
+    history.push(text);
+    console.log(history);
+
+    res.send({ response: text });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+module.exports = { generateResponse };
